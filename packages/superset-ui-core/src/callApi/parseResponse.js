a19871ca4f1c68dcc6d3753468e1d117b2d6be64
Chris Williams <chris.williams@airbnb.com>
2018-09-13 12:47:41 -0700

    [core] clean up parseResponse

--- packages/superset-ui-core/src/callApi/parseResponse.js
+++ packages/superset-ui-core/src/callApi/parseResponse.js
@@ -1,35 +1,29 @@
+const PARSERS = {
+  json: response =>
+    // first try to parse as json, and fall back to text (e.g., in the case of HTML stacktrace)
+    // cannot fall back to .text() without cloning the response because body is single-use
+    response
+      .clone()
+      .json()
+      .then(json => ({ json, response }))
+      .catch(() => /* jsonParseError */ response.text().then(text => ({ response, text }))),
+
+  text: response => response.text().then(text => ({ response, text })),
+};
+
 export default function parseResponse(apiPromise, parseMethod = 'json') {
-  return apiPromise.then(response => {
-    let parsedPromise;
-    if (parseMethod === 'json') {
-      // first try to parse as json, and fall back to text (e.g., in the case of HTML stacktrace)
-      // cannot fall back to .text() without cloning the response because body is single-use
-      parsedPromise = response
-        .clone()
-        .json()
-        .catch(() => /* jsonParseError */ response.text().then(textPayload => ({ textPayload })));
-    } else if (parseMethod === 'text') {
-      parsedPromise = response.text().then(textPayload => ({ textPayload }));
-    } else {
-      throw Error(`Unrecognized parseMethod '${parseMethod}', expected 'json' or 'text'`);
+  const responseParser = PARSERS[parseMethod] || PARSERS.json;
+
+  return apiPromise.then(responseParser).then(({ json, text, response }) => {
+    // HTTP 404 or 500 are not rejected, ok is just set to false
+    if (!response.ok) {
+      return Promise.reject({
+        error: response.error || (json && json.error) || text || 'An error occurred',
+        status: response.status,
+        statusText: response.statusText,
+      });
     }
 
-    return parsedPromise
-      .then(maybeJson => ({
-        json: maybeJson.textPayload ? undefined : maybeJson,
-        text: maybeJson.textPayload,
-      }))
-      .then(({ json, text }) => {
-        // HTTP 404 or 500 are not rejected, ok is just set to false
-        if (!response.ok) {
-          return Promise.reject({
-            error: response.error || (json && json.error) || text || 'An error occurred',
-            status: response.status,
-            statusText: response.statusText,
-          });
-        }
-
-        return typeof text === 'undefined' ? { json, response } : { response, text };
-      });
+    return typeof text === 'undefined' ? { json, response } : { response, text };
   });
 }
