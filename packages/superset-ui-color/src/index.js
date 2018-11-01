4d061df87dee428ffda87f0978d0beaaf8ee976c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-01 15:40:12 -0700

    Define top-level exports

--- packages/superset-ui-color/src/index.js
+++ packages/superset-ui-color/src/index.js
@@ -1,2 +1,8 @@
-const x = 1;
-export default x;
+import * as CategoricalColorNamespace from './CategoricalColorNamespace';
+
+export { CategoricalColorNamespace };
+export { default as CategoricalColorScale } from './CategoricalColorScale';
+export { default as CategoricalScheme } from './CategoricalScheme';
+export { default as getCategoricalSchemeRegistry } from './CategoricalSchemeRegistrySingleton';
+export { default as getSequentialSchemeRegistry } from './SequentialSchemeRegistrySingleton';
+export { default as SequentialScheme } from './SequentialScheme';
