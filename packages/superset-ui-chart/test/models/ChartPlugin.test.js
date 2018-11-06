a38916f3a74dd2579a130dbff94dcf6d44948f9e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-05 19:15:33 -0800

    add unit test for ChartPlugin

--- packages/superset-ui-chart/test/models/ChartPlugin.test.js
+++ packages/superset-ui-chart/test/models/ChartPlugin.test.js
@@ -19,8 +19,77 @@ describe('ChartPlugin', () => {
     it('throws an error if metadata is not specified', () => {
       expect(() => new ChartPlugin()).toThrowError(Error);
     });
-    it('throws an error if none of Chart or loadChart is specified', () => {
-      expect(() => new ChartPlugin({ metadata })).toThrowError(Error);
+    describe('buildQuery', () => {
+      const FORM_DATA = { field: 1 };
+      it('defaults to identity function', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+        });
+        expect(plugin.loadBuildQuery()(FORM_DATA)).toBe(FORM_DATA);
+      });
+      it('uses loadBuildQuery field if specified', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+          loadBuildQuery: () => () => ({ field2: 2 }),
+        });
+        expect(plugin.loadBuildQuery()(FORM_DATA)).toEqual({ field2: 2 });
+      });
+      it('uses buildQuery field if specified', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+          buildQuery: () => ({ field2: 2 }),
+        });
+        expect(plugin.loadBuildQuery()(FORM_DATA)).toEqual({ field2: 2 });
+      });
+    });
+    describe('Chart', () => {
+      it('uses loadChart if specified', () => {
+        const loadChart = () => 'test';
+        const plugin = new ChartPlugin({
+          metadata,
+          loadChart,
+        });
+        expect(plugin.loadChart).toBe(loadChart);
+      });
+      it('uses Chart field if specified', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+        });
+        expect(plugin.loadChart()).toEqual('test');
+      });
+      it('throws an error if none of Chart or loadChart is specified', () => {
+        expect(() => new ChartPlugin({ metadata })).toThrowError(Error);
+      });
+    });
+    describe('transformProps', () => {
+      const PROPS = { field: 1 };
+      it('defaults to identity function', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+        });
+        expect(plugin.loadTransformProps()(PROPS)).toBe(PROPS);
+      });
+      it('uses loadTransformProps field if specified', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+          loadTransformProps: () => () => ({ field2: 2 }),
+        });
+        expect(plugin.loadTransformProps()(PROPS)).toEqual({ field2: 2 });
+      });
+      it('uses transformProps field if specified', () => {
+        const plugin = new ChartPlugin({
+          metadata,
+          Chart: 'test',
+          transformProps: () => ({ field2: 2 }),
+        });
+        expect(plugin.loadTransformProps()(PROPS)).toEqual({ field2: 2 });
+      });
     });
   });
 
