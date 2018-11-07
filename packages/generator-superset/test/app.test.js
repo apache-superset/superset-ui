2041ec949a881fd84dd64a43b38e188a3711559b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-06 22:10:23 -0800

    add afterall to correct working directory

--- packages/generator-superset/test/app.test.js
+++ packages/generator-superset/test/app.test.js
@@ -3,10 +3,21 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-superset:app', () => {
-  beforeAll(() =>
-    helpers.run(path.join(__dirname, '../generators/app')).withPrompts({ name: 'my-package' }));
+  let dir;
+
+  beforeAll(() => {
+    dir = process.cwd();
+
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ name: 'my-package' });
+  });
 
   it('creates files', () => {
     assert.file(['package.json', 'README.md', 'src/index.js', 'test/index.test.js']);
+  });
+
+  afterAll(() => {
+    process.chdir(dir);
   });
 });
--- packages/generator-superset/test/package.test.js
+++ packages/generator-superset/test/package.test.js
@@ -3,13 +3,22 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-superset:package', () => {
-  beforeAll(() =>
-    helpers
+  let dir;
+
+  beforeAll(() => {
+    dir = process.cwd();
+
+    return helpers
       .run(path.join(__dirname, '../generators/package'))
       .withPrompts({ name: 'my-package' })
-      .withOptions({ skipInstall: true }));
+      .withOptions({ skipInstall: true });
+  });
 
   it('creates files', () => {
     assert.file(['package.json', 'README.md', 'src/index.js', 'test/index.test.js']);
+  });
+
+  afterAll(() => {
+    process.chdir(dir);
   });
 });
