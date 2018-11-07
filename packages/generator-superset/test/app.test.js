c66fdf174fe253745ffa81b5c6eedd1a3d920270
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-07 11:10:44 -0800

    add readme

--- packages/generator-superset/test/app.test.js
+++ packages/generator-superset/test/app.test.js
@@ -17,6 +17,17 @@ describe('generator-superset:app', () => {
     assert.file(['package.json', 'README.md', 'src/index.js', 'test/index.test.js']);
   });
 
+  /*
+   * Change working directory back to original working directory
+   * after the test has completed.
+   * yeoman tests switch to tmp directory and write files there.
+   * Usually this is fine for solo package.
+   * However, for a monorepo like this one,
+   * it made jest confuses with current directory
+   * (being in tmp directory instead of superset-ui root)
+   * and interferes with other tests in sibling packages
+   * that are run after the yeoman tests.
+   */
   afterAll(() => {
     process.chdir(dir);
   });
