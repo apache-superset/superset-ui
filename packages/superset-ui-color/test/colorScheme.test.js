4429fd4a240b9ca4bd4159375e5b4875f6764f04
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-08 14:17:22 -0800

    add more unit tests for color package

--- packages/superset-ui-color/test/colorScheme.test.js
+++ packages/superset-ui-color/test/colorScheme.test.js
@@ -1,27 +1,15 @@
-import categoricalAirbnb from '../src/colorSchemes/categorical/airbnb';
-import categoricalD3 from '../src/colorSchemes/categorical/d3';
-import categoricalGoogle from '../src/colorSchemes/categorical/google';
-import categoricalLyft from '../src/colorSchemes/categorical/lyft';
-import sequentialCommon from '../src/colorSchemes/sequential/common';
-import sequentialD3 from '../src/colorSchemes/sequential/d3';
-import CategoricalScheme from '../src/CategoricalScheme';
-import SequentialScheme from '../src/SequentialScheme';
+import ColorScheme from '../src/ColorScheme';
 
-describe('Color Schemes', () => {
-  describe('categorical', () => {
-    it('returns an array of CategoricalScheme', () => {
-      [categoricalAirbnb, categoricalD3, categoricalGoogle, categoricalLyft].forEach(group => {
-        expect(group).toBeInstanceOf(Array);
-        group.forEach(scheme => expect(scheme).toBeInstanceOf(CategoricalScheme));
-      });
+describe('ColorScheme', () => {
+  describe('new ColorScheme()', () => {
+    it('requires name and color', () => {
+      expect(() => new ColorScheme()).toThrow();
+      expect(() => new ColorScheme({ name: 'test' })).toThrow();
+      expect(() => new ColorScheme({ colors: ['red', 'blue'] })).toThrow();
     });
-  });
-  describe('sequential', () => {
-    it('returns an array of SequentialScheme', () => {
-      [sequentialCommon, sequentialD3].forEach(group => {
-        expect(group).toBeInstanceOf(Array);
-        group.forEach(scheme => expect(scheme).toBeInstanceOf(SequentialScheme));
-      });
+    it('returns an instance of ColorScheme', () => {
+      const scheme = new ColorScheme({ name: 'test', colors: ['red', 'blue'] });
+      expect(scheme).toBeInstanceOf(ColorScheme);
     });
   });
 });
