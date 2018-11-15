fdcfeced67e9429d57db27c2787e890146f35067
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-14 23:36:56 -0800

    use ternary

--- packages/superset-ui-number-format/src/formatters/siAtMost3Digit.js
+++ packages/superset-ui-number-format/src/formatters/siAtMost3Digit.js
@@ -9,13 +9,10 @@ const formatter = new NumberFormatter({
   formatName: 'si_at_most_3_digit',
   label: 'SI with at most 3 significant digits',
   formatFunc(n) {
-    let si = siFormatter(n);
-    // Removing trailing `.00` if any
-    if (si.slice(-1) < 'A') {
-      si = parseFloat(si).toString();
-    }
+    const si = siFormatter(n);
 
-    return si;
+    // Removing trailing `.00` if any
+    return si.slice(-1) < 'A' ? parseFloat(si).toString() : si;
   },
 });
 
