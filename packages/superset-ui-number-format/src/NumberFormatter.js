6368a290ae3d210ca4be0c6b2f91475303a6e8fb
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-14 16:06:02 -0800

    use ExtensibleFunction from core

--- packages/superset-ui-number-format/src/NumberFormatter.js
+++ packages/superset-ui-number-format/src/NumberFormatter.js
@@ -1,17 +1,8 @@
 import isString from 'lodash/isString';
 import { format as d3Format } from 'd3-format';
-import { isRequired } from '@superset-ui/core';
+import { ExtensibleFunction, isRequired } from '@superset-ui/core';
 
 export const PREVIEW_VALUE = 12345.432;
-
-/**
- * From https://stackoverflow.com/questions/36871299/how-to-extend-function-with-es6-classes
- */
-class ExtensibleFunction extends Function {
-  constructor(fn) {
-    return Object.setPrototypeOf(fn, new.target.prototype);
-  }
-}
 
 export default class NumberFormatter extends ExtensibleFunction {
   constructor(configOrFormatString = isRequired('configOrFormatString')) {
