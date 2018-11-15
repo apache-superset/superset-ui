879902eb8623846fdbd37ec4cef088c58895da49
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-14 23:37:56 -0800

    test 0

--- packages/superset-ui-number-format/test/formatters/siAtMost3Digit.test.js
+++ packages/superset-ui-number-format/test/formatters/siAtMost3Digit.test.js
@@ -14,7 +14,7 @@ describe('Formatter: si_at_most_3_digit', () => {
     expect(formatter(10100)).toBe('10.1k');
     expect(formatter(111000000)).toBe('111M');
     expect(formatter(0.23)).toBe('230m');
-
+    expect(formatter(0)).toBe('0');
     expect(formatter(-10)).toBe('-10');
     expect(formatter(-1)).toBe('-1');
     expect(formatter(-1.0)).toBe('-1');
