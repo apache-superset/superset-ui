2893ec31bb9427779345317c68ed03cbed7b1fd6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-29 11:28:49 -0800

    explicitly disable magic number

--- packages/superset-ui-time-format/src/TimeFormatter.js
+++ packages/superset-ui-time-format/src/TimeFormatter.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-magic-numbers */
+
 import { ExtensibleFunction, isRequired } from '@superset-ui/core';
 
-// eslint-disable-next-line
 export const PREVIEW_TIME = new Date(Date.UTC(2017, 1, 14, 11, 22, 33));
 
 export default class TimeFormatter extends ExtensibleFunction {
