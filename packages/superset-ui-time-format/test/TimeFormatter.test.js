9a82ed9c626b6a586bb1435f62b5257e6e5a0a6b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-29 11:25:20 -0800

    Update preview test

--- packages/superset-ui-time-format/test/TimeFormatter.test.js
+++ packages/superset-ui-time-format/test/TimeFormatter.test.js
@@ -48,7 +48,8 @@ describe('TimeFormatter', () => {
       formatFunc: value => `${value.getFullYear()}`,
     });
     it('returns string comparing value before and after formatting', () => {
-      expect(formatter.preview(PREVIEW_TIME)).toEqual('Tue, 14 Feb 2017 11:22:33 GMT => 2017');
+      const time = new Date(Date.UTC(2018, 10, 21, 22, 11, 44));
+      expect(formatter.preview(time)).toEqual('Wed, 21 Nov 2018 22:11:44 GMT => 2018');
     });
     it('uses the default preview value if not specified', () => {
       expect(formatter.preview()).toEqual('Tue, 14 Feb 2017 11:22:33 GMT => 2017');
