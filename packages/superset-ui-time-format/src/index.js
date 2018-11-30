e6e51864cdf16fc4d943adb0317cf00e084cf9ab
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-30 10:26:03 -0800

    update NumberFormats and TimeFormats export and update change log

--- packages/superset-ui-time-format/src/index.js
+++ packages/superset-ui-time-format/src/index.js
@@ -1,14 +1,11 @@
-import * as TimeFormats from './TimeFormats';
-
-export { TimeFormats };
+export { default as TimeFormats, LOCAL_PREFIX } from './TimeFormats';
+export { default as TimeFormatter, PREVIEW_TIME } from './TimeFormatter';
 
 export {
   default as getTimeFormatterRegistry,
   formatTime,
   getTimeFormatter,
 } from './TimeFormatterRegistrySingleton';
-
-export { default as TimeFormatter, PREVIEW_TIME } from './TimeFormatter';
 
 export { default as createD3TimeFormatter } from './factories/createD3TimeFormatter';
 export { default as createMultiFormatter } from './factories/createMultiFormatter';
