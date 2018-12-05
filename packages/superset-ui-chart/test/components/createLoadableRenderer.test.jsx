a096f97df139a89870535f41ece219e9b6237635
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-12-05 14:21:44 -0800

    remove unnecessary enzyme config

--- packages/superset-ui-chart/test/components/createLoadableRenderer.test.jsx
+++ packages/superset-ui-chart/test/components/createLoadableRenderer.test.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import createLoadableRenderer from '../../src/components/createLoadableRenderer';
-
-configure({ adapter: new Adapter() });
 
 describe('createLoadableRenderer', () => {
   function TestComponent() {
