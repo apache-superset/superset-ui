19818b0774590cc6239a8946a01bb87b5d2cc150
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-12-06 12:00:33 -0800

    convert ChartClient to TypeScript

--- packages/superset-ui-chart/src/clients/ChartClient.ts
+++ packages/superset-ui-chart/src/clients/ChartClient.ts
@@ -1,17 +1,35 @@
 import { isDefined } from '@superset-ui/core';
-import { SupersetClient } from '@superset-ui/connection';
+import { SupersetClient, RequestConfig } from '@superset-ui/connection';
 import getChartBuildQueryRegistry from '../registries/ChartBuildQueryRegistrySingleton';
+import { FormData, AnnotationLayerMetadata } from '../query';
+
+interface SliceIdAndOrFormData {
+  sliceId?: number;
+  formData: FormData;
+}
+
+interface AnnotationData {
+  [key: string]: object;
+}
+
+interface ChartData {
+  annotationData: AnnotationData;
+  datasource: object;
+  queryData: object;
+}
 
 export default class ChartClient {
-  constructor({ client = SupersetClient }) {
+  client: SupersetClient;
+
+  constructor({ client = SupersetClient } = {}) {
     this.client = client;
   }
 
-  loadFormData({ sliceId, formData }, options) {
+  loadFormData(input: SliceIdAndOrFormData, options?: RequestConfig): Promise<FormData> {
     /* If sliceId is provided, use it to fetch stored formData from API */
-    if (sliceId) {
+    if (input.sliceId) {
       const promise = this.client.get({
-        endpoint: `/superset/slice_json/${sliceId}`,
+        endpoint: `/superset/slice_json/${input.sliceId}`,
         ...options,
       });
 
@@ -19,21 +37,21 @@ export default class ChartClient {
        * If formData is also specified, override API result
        * with user-specified formData
        */
-      return formData
-        ? promise.then(dbFormData => ({
+      return input.formData
+        ? promise.then((dbFormData: object) => ({
             ...dbFormData,
-            formData,
+            ...input.formData,
           }))
         : promise;
     }
 
     /* If sliceId is not provided, returned formData wrapped in a Promise */
-    return formData
-      ? Promise.resolve(formData)
+    return input.formData
+      ? Promise.resolve(input.formData)
       : Promise.reject(new Error('At least one of sliceId or formData must be specified'));
   }
 
-  loadQueryData(formData, options) {
+  loadQueryData(formData: FormData, options?: RequestConfig) {
     const buildQuery = getChartBuildQueryRegistry().get(formData.viz_type);
     if (buildQuery) {
       return this.client.post({
@@ -46,28 +64,28 @@ export default class ChartClient {
     return Promise.reject(new Error(`Unknown chart type: ${formData.viz_type}`));
   }
 
-  loadDatasource(datasourceKey, options) {
+  loadDatasource(datasourceKey: string, options?: RequestConfig) {
     return this.client.get({
       endpoint: `/superset/fetch_datasource_metadata?datasourceKey=${datasourceKey}`,
       ...options,
     });
   }
 
-  loadAnnotation(annotationLayer) {
+  loadAnnotation(annotationLayer: AnnotationLayerMetadata): Promise<object> {
     /* When annotation does not require query */
     if (!isDefined(annotationLayer.sourceType)) {
-      return Promise.resolve();
+      return Promise.resolve({});
     }
 
     // TODO: Implement
     return Promise.reject(new Error('This feature is not implemented yet.'));
   }
 
-  loadAnnotations(annotationLayers) {
+  loadAnnotations(annotationLayers?: Array<AnnotationLayerMetadata>): Promise<AnnotationData> {
     if (Array.isArray(annotationLayers) && annotationLayers.length > 0) {
       return Promise.all(annotationLayers.map(layer => this.loadAnnotation(layer))).then(results =>
         annotationLayers.reduce((prev, layer, i) => {
-          const output = prev;
+          const output: AnnotationData = prev;
           output[layer.name] = results[i];
 
           return output;
@@ -78,13 +96,13 @@ export default class ChartClient {
     return Promise.resolve({});
   }
 
-  loadChartData({ sliceId, formData }) {
-    return this.loadFormData({ formData, sliceId }).then(finalFormData =>
+  loadChartData(input: SliceIdAndOrFormData): Promise<ChartData> {
+    return this.loadFormData(input).then(finalFormData =>
       Promise.all([
-        this.loadQueryData(finalFormData),
         this.loadAnnotations(finalFormData.annotation_layers),
         this.loadDatasource(finalFormData.datasource),
-      ]).then(([queryData, datasource, annotationData]) => ({
+        this.loadQueryData(finalFormData),
+      ]).then(([annotationData, datasource, queryData]) => ({
         annotationData,
         datasource,
         queryData,
