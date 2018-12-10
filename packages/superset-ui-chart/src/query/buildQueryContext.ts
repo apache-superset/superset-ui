f90d0a6d7078f8c8b9ef43220bfbafb967be4644
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-12-10 14:14:42 -0800

    Convert ChartPlugin to ts

--- packages/superset-ui-chart/src/query/buildQueryContext.ts
+++ packages/superset-ui-chart/src/query/buildQueryContext.ts
@@ -1,14 +1,21 @@
 import buildQueryObject, { QueryObject } from './buildQueryObject';
-import { DatasourceKey } from './DatasourceKey';
+import { DatasourceKey, DatasourceType } from './DatasourceKey';
 import FormData from './FormData';
 
 const WRAP_IN_ARRAY = (baseQueryObject: QueryObject) => [baseQueryObject];
 
-// Note: let TypeScript infer the return type
+export interface QueryContext {
+  datasource: {
+    id: number;
+    type: DatasourceType;
+  };
+  queries: Array<QueryObject>;
+}
+
 export default function buildQueryContext(
   formData: FormData,
   buildQuery: (baseQueryObject: QueryObject) => QueryObject[] = WRAP_IN_ARRAY,
-) {
+): QueryContext {
   return {
     datasource: new DatasourceKey(formData.datasource).toObject(),
     queries: buildQuery(buildQueryObject(formData)),
