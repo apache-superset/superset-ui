424f7b67818c5057ee398da30461d505abd13ee8
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-20 11:48:21 -0800

    test: add unit tests

--- packages/superset-ui-number-format/src/factories/createSmartNumberFormatter.ts
+++ packages/superset-ui-number-format/src/factories/createSmartNumberFormatter.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-magic-numbers */
+
 import { format as d3Format } from 'd3-format';
 import NumberFormatter from '../NumberFormatter';
 
@@ -12,29 +14,28 @@ export default function createSmartNumberFormatter(
   const { description, signed = false, id, label } = config;
   const siFormatter = d3Format(`.3~s`);
   const float2PointFormatter = d3Format(`.2~f`);
-  const float3PointFormatter = d3Format(`.3~f`);
+  const float4PointFormatter = d3Format(`.4~f`);
 
-  const getSign = signed ? () => '' : (value: number) => (value > 0 ? '+' : '');
+  const getSign = signed ? (value: number) => (value > 0 ? '+' : '') : () => '';
 
   function formatValue(value: number) {
     if (value === 0) {
       return '0';
-    } else {
-      const absoluteValue = Math.abs(value);
-      if (absoluteValue >= 1000) {
-        // Normal human being are more familiar
-        // with billion (B) that giga (G)
-        return siFormatter(value).replace('G', 'B');
-      } else if (absoluteValue >= 1) {
-        return float2PointFormatter(value);
-      } else if (absoluteValue >= 0.001) {
-        return float3PointFormatter(value);
-      } else if (absoluteValue > 0.000001) {
-        return `${siFormatter(value * 100000)}µ`;
-      }
-
-      return siFormatter(value);
     }
+    const absoluteValue = Math.abs(value);
+    if (absoluteValue >= 1000) {
+      // Normal human being are more familiar
+      // with billion (B) that giga (G)
+      return siFormatter(value).replace('G', 'B');
+    } else if (absoluteValue >= 1) {
+      return float2PointFormatter(value);
+    } else if (absoluteValue >= 0.001) {
+      return float4PointFormatter(value);
+    } else if (absoluteValue > 0.000001) {
+      return `${siFormatter(value * 1000000)}µ`;
+    }
+
+    return siFormatter(value);
   }
 
   return new NumberFormatter({
