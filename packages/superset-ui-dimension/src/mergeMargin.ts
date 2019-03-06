6ec997971f8b3a7b372a1f17c13792d8ff126126
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-03-06 15:14:11 -0800

    refactor: address chris' comments

--- packages/superset-ui-dimension/src/mergeMargin.ts
+++ packages/superset-ui-dimension/src/mergeMargin.ts
@@ -8,17 +8,12 @@ export default function mergeMargin(
   const { top = 0, left = 0, bottom = 0, right = 0 } = margin1;
   const { top: top2 = 0, left: left2 = 0, bottom: bottom2 = 0, right: right2 = 0 } = margin2;
 
-  return mode === 'expand'
-    ? {
-        bottom: Math.max(bottom, bottom2),
-        left: Math.max(left, left2),
-        right: Math.max(right, right2),
-        top: Math.max(top, top2),
-      }
-    : {
-        bottom: Math.min(bottom, bottom2),
-        left: Math.min(left, left2),
-        right: Math.min(right, right2),
-        top: Math.min(top, top2),
-      };
+  const func = mode === 'expand' ? Math.max : Math.min;
+
+  return {
+    bottom: func(bottom, bottom2),
+    left: func(left, left2),
+    right: func(right, right2),
+    top: func(top, top2),
+  };
 }
