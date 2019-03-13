dc27eff5b382633481a2b7fa121bea8baa3f7a7f
Chris Williams <chris_williams@airbnb.com>
2019-03-13 12:32:37 -0700

    docs: [demo] add custom webpack config to resolve TS files

---
+++ packages/superset-ui-demo/storybook-config/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = (baseConfig, env, config) => {
+  const customConfig = config;
+
+  customConfig.module.rules.push({
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    },
+    test: /\.tsx?$/,
+  });
+
+  customConfig.resolve.extensions.push('.ts', '.tsx');
+
+  return customConfig;
+};
