8820be26089d5f47db6738cdce1b417af7c5fa92
Conglei Shi <conglei.shi@airbnb.com>
2019-03-24 18:16:02 -0700

    fix: 🐛 Fixed typ definiton for chart plugin
    This PR is to fix the type definiton for transformProps and LoadData.

--- packages/superset-ui-chart/src/types/Query.ts
+++ packages/superset-ui-chart/src/types/Query.ts
@@ -38,8 +38,8 @@ export interface PlainProps {
 
 type TransformFunction<Input = PlainProps, Output = PlainProps> = (x: Input) => Output;
 
-export type PreTransformProps = TransformFunction<ChartProps>;
-export type TransformProps = TransformFunction;
+export type PreTransformProps = TransformFunction<ChartProps, ChartProps | PlainProps>;
+export type TransformProps = TransformFunction<ChartProps | PlainProps>;
 export type PostTransformProps = TransformFunction;
 
 export type BuildQueryFunction<T extends ChartFormData> = (formData: T) => QueryContext;
