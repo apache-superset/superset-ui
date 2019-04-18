ca282888bd3f4efcd6e5e7a0696350089803e585
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-17 18:03:38 -0700

    feat: add more type to chartprops

--- packages/superset-ui-chart/src/models/ChartProps.ts
+++ packages/superset-ui-chart/src/models/ChartProps.ts
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 import { convertKeysToCamelCase } from '@superset-ui/core';
+import { ChartFormData } from '../types/ChartFormData';
 
 interface PlainObject {
   [key: string]: any;
@@ -14,13 +15,13 @@ type SnakeCaseFormData = PlainObject;
 export type QueryData = PlainObject;
 type Filters = any[];
 type HandlerFunction = (...args: any[]) => void;
-type ChartPropsSelector = (c: ChartPropsConfig) => ChartProps;
+type ChartPropsSelector<FormDataType> = (c: ChartPropsConfig<FormDataType>) => ChartProps;
 
-interface ChartPropsConfig {
+interface ChartPropsConfig<FormDataType> {
   annotationData?: AnnotationData;
   datasource?: SnakeCaseDatasource;
   filters?: Filters;
-  formData?: SnakeCaseFormData;
+  formData: FormDataType;
   height?: number;
   onAddFilter?: HandlerFunction;
   onError?: HandlerFunction;
@@ -35,15 +36,57 @@ function NOOP() {}
 const DEFAULT_WIDTH = 800;
 const DEFAULT_HEIGHT = 600;
 
-export default class ChartProps {
-  static createSelector: () => ChartPropsSelector;
+export default class ChartProps<FormDataType extends ChartFormData = ChartFormData> {
+  static createSelector = function create<FormDataType extends ChartFormData>(): ChartPropsSelector<
+    FormDataType
+  > {
+    return createSelector(
+      (input: ChartPropsConfig<FormDataType>) => input.annotationData,
+      input => input.datasource,
+      input => input.filters,
+      input => input.formData,
+      input => input.height,
+      input => input.onAddFilter,
+      input => input.onError,
+      input => input.payload,
+      input => input.setControlValue,
+      input => input.setTooltip,
+      input => input.width,
+      (
+        annotationData,
+        datasource,
+        filters,
+        formData,
+        height,
+        onAddFilter,
+        onError,
+        payload,
+        setControlValue,
+        setTooltip,
+        width,
+      ) =>
+        new ChartProps({
+          annotationData,
+          datasource,
+          filters,
+          formData,
+          height,
+          onAddFilter,
+          onError,
+          payload,
+          setControlValue,
+          setTooltip,
+          width,
+        }),
+    );
+  };
 
   annotationData: AnnotationData;
   datasource: CamelCaseDatasource;
   rawDatasource: SnakeCaseDatasource;
   filters: Filters;
   formData: CamelCaseFormData;
-  rawFormData: SnakeCaseFormData;
+  rawFormData: FormDataType;
   height: number;
   onAddFilter: HandlerFunction;
   onError: HandlerFunction;
@@ -52,12 +95,12 @@ export default class ChartProps {
   setTooltip: HandlerFunction;
   width: number;
 
-  constructor(config: ChartPropsConfig = {}) {
+  constructor(config: ChartPropsConfig<FormDataType>) {
     const {
       annotationData = {},
       datasource = {},
       filters = [],
-      formData = {},
+      formData,
       onAddFilter = NOOP,
       onError = NOOP,
       payload = {},
@@ -81,45 +124,3 @@ export default class ChartProps {
     this.setTooltip = setTooltip;
   }
 }
-
-ChartProps.createSelector = function create(): ChartPropsSelector {
-  return createSelector(
-    (input: ChartPropsConfig) => input.annotationData,
-    input => input.datasource,
-    input => input.filters,
-    input => input.formData,
-    input => input.height,
-    input => input.onAddFilter,
-    input => input.onError,
-    input => input.payload,
-    input => input.setControlValue,
-    input => input.setTooltip,
-    input => input.width,
-    (
-      annotationData,
-      datasource,
-      filters,
-      formData,
-      height,
-      onAddFilter,
-      onError,
-      payload,
-      setControlValue,
-      setTooltip,
-      width,
-    ) =>
-      new ChartProps({
-        annotationData,
-        datasource,
-        filters,
-        formData,
-        height,
-        onAddFilter,
-        onError,
-        payload,
-        setControlValue,
-        setTooltip,
-        width,
-      }),
-  );
-};
