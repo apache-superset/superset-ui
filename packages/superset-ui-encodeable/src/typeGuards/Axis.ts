5883880afdf9266529cc74fd791c576075cbfb14
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-08-12 18:12:08 -0700

    fix: remove unused

--- packages/superset-ui-encodeable/src/typeGuards/Axis.ts
+++
@@ -1,6 +0,0 @@
-import { Axis } from '../types/Axis';
-
-// eslint-disable-next-line import/prefer-default-export
-export function isAxis(axis: Axis | null | undefined | false): axis is Axis {
-  return axis !== false && axis !== null && axis !== undefined;
-}
