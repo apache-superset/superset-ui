24e5a14450d469dd7dddfbb8cabf28d44d5a35cb
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-08-19 11:42:13 -0700

    fix: update type

--- packages/superset-ui-encodeable/src/parsers/createGetterFromChannelDef.ts
+++ packages/superset-ui-encodeable/src/parsers/createGetterFromChannelDef.ts
@@ -2,8 +2,11 @@ import { get } from 'lodash/fp';
 import identity from '../utils/identity';
 import { ChannelDef } from '../types/ChannelDef';
 import { isValueDef } from '../typeGuards/ChannelDef';
+import { PlainObject } from '../types/Data';
 
-export default function createGetterFromChannelDef(definition: ChannelDef): (x?: any) => any {
+export default function createGetterFromChannelDef(
+  definition: ChannelDef,
+): (x?: PlainObject) => any {
   if (isValueDef(definition)) {
     return () => definition.value;
   } else if (typeof definition.field !== 'undefined') {
