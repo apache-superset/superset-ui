b8e0e4c3601e369f17cb531171e6614496fb3878
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-02-15 00:07:04 -0800

    fix: type

--- packages/superset-ui-core/src/utils/convertKeysToCamelCase.ts
+++ packages/superset-ui-core/src/utils/convertKeysToCamelCase.ts
@@ -2,14 +2,16 @@ import camelCase from 'lodash-es/camelCase';
 import isPlainObject from 'lodash-es/isPlainObject';
 import mapKeys from 'lodash-es/mapKeys';
 
-export default function convertKeysToCamelCase(object: unknown) {
+// TODO: Change any to unknown and fix the consequences
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type PlainObject = { [key: string]: any };
+
+export default function convertKeysToCamelCase<T>(object: T): T {
   if (object === null || object === undefined) {
     return object;
   }
   if (isPlainObject(object)) {
-    // TODO: Change any to unknown and fix the consequences
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return mapKeys(object as { [key: string]: any }, (_, k) => camelCase(k));
+    return mapKeys(object as PlainObject, (_, k) => camelCase(k)) as T;
   }
   throw new Error(`Cannot convert input that is not a plain object: ${object}`);
 }
