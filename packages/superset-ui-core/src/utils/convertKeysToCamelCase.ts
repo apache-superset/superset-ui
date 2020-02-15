a153b8047764e4bf21d4454986e022de0ed3fe05
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-02-14 22:10:37 -0800

    fix: type

--- packages/superset-ui-core/src/utils/convertKeysToCamelCase.ts
+++ packages/superset-ui-core/src/utils/convertKeysToCamelCase.ts
@@ -7,7 +7,9 @@ export default function convertKeysToCamelCase(object: unknown) {
     return object;
   }
   if (isPlainObject(object)) {
-    return mapKeys(object as { [key: string]: unknown }, (_, k) => camelCase(k));
+    // TODO: Change any to unknown and fix the consequences
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return mapKeys(object as { [key: string]: any }, (_, k) => camelCase(k));
   }
   throw new Error(`Cannot convert input that is not a plain object: ${object}`);
 }
