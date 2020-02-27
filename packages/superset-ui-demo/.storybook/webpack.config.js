09aa1d5d115810211fdda268c9593ab51b14b2d6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-02-27 14:57:40 -0800

    build: fix demo build

--- packages/superset-ui-demo/.storybook/webpack.config.js
+++ packages/superset-ui-demo/.storybook/webpack.config.js
@@ -1,17 +1,45 @@
 module.exports = async ({ config }) => {
+  config.module.rules[0].use[0].options.presets = [
+    ['@babel/preset-env', {
+      useBuiltIns: 'usage',
+      corejs: 3,
+      loose: true,
+      modules: false,
+      shippedProposals: true,
+      targets: false
+    }],
+    '@babel/preset-react',
+  ];
+  config.module.rules[0].use[0].options.plugins = [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    ["@babel/plugin-transform-runtime", { "corejs": 3 }]
+  ];
+
   config.module.rules.push({
-    loader: require.resolve('babel-loader'),
-    options: {
-      presets: [
-        ['@babel/preset-env', { useBuiltIns: 'entry' }],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
-      ],
-      plugins: [
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-syntax-dynamic-import',
-      ],
+    use: {
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            useBuiltIns: 'usage',
+            corejs: 3,
+            loose: true,
+            modules: false,
+            shippedProposals: true,
+            targets: false
+          }],
+          '@babel/preset-react',
+          '@babel/preset-typescript',
+        ],
+        plugins: [
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-syntax-dynamic-import',
+          ["@babel/plugin-transform-runtime", { "corejs": 3 }]
+        ],
+      }
     },
     test: /\.tsx?$/,
     exclude: /node_modules/,
