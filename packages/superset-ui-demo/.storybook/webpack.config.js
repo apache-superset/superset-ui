1139b5507a1e06897024bff633078563913e565f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-02-27 23:17:30 -0800

    fix: clean up webpack config

--- packages/superset-ui-demo/.storybook/webpack.config.js
+++ packages/superset-ui-demo/.storybook/webpack.config.js
@@ -1,48 +1,51 @@
-module.exports = async ({ config }) => {
-  config.module.rules[0].use[0].options.presets = [
-    ['@babel/preset-env', {
-      useBuiltIns: 'usage',
-      corejs: 3,
-      loose: true,
-      modules: false,
-      shippedProposals: true,
-      targets: false
-    }],
-    '@babel/preset-react',
-  ];
-  config.module.rules[0].use[0].options.plugins = [
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-syntax-dynamic-import',
-    ["@babel/plugin-transform-runtime", { "corejs": 3 }]
-  ];
+const path = require('path');
 
+const jsBabelPresets = [
+  ['@babel/preset-env', {
+    useBuiltIns: 'usage',
+    corejs: 3,
+    loose: true,
+    modules: false,
+    shippedProposals: true,
+    targets: false
+  }],
+  '@babel/preset-react',
+];
+
+const tsBabelPresets = jsBabelPresets.concat(['@babel/preset-typescript']);
+
+const babelPlugins = [
+  '@babel/plugin-proposal-object-rest-spread',
+  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-syntax-dynamic-import',
+  ["@babel/plugin-transform-runtime", { "corejs": 3 }]
+];
+
+module.exports = async ({ config }) => {
+  const cacheDirectory = path.resolve('../../../node_modules/.cache/storybook');
+
+  // rule that applies to jsx? files
+  config.module.rules[0].use = {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory,
+      presets: jsBabelPresets,
+      plugins: babelPlugins,
+    }
+  };
+
+  // add rule for handling typescript
   config.module.rules.push({
-    use: {
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [
-          ['@babel/preset-env', {
-            useBuiltIns: 'usage',
-            corejs: 3,
-            loose: true,
-            modules: false,
-            shippedProposals: true,
-            targets: false
-          }],
-          '@babel/preset-react',
-          '@babel/preset-typescript',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-syntax-dynamic-import',
-          ["@babel/plugin-transform-runtime", { "corejs": 3 }]
-        ],
-      }
-    },
     test: /\.tsx?$/,
     exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory,
+        presets: tsBabelPresets,
+        plugins: babelPlugins,
+      }
+    },
   });
 
   config.resolve.extensions.push('.ts', '.tsx');
