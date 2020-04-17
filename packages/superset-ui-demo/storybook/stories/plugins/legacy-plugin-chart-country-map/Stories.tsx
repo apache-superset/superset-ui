7e982e5c6d9b66e36b383bda20571e8d45544e75
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 16:12:14 -0700

    docs: wire storybook

--- packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-country-map/Stories.tsx
+++ packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-country-map/Stories.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable no-magic-numbers */
 import React from 'react';
 import { SuperChart } from '@superset-ui/chart';
-import { select } from '@storybook/addon-knobs';
 import CountryMapChartPlugin from '@superset-ui/legacy-plugin-chart-country-map';
 import countries from '@superset-ui/legacy-plugin-chart-country-map/esm/countries';
+import { withKnobs, select } from '@storybook/addon-knobs';
 import data from './data';
 
 new CountryMapChartPlugin().configure({ key: 'country-map' }).register();
 
 export default {
   title: 'Legacy Chart Plugins|legacy-plugin-chart-country-map',
+  decorators: [withKnobs],
 };
 
 export const basic = () => (
