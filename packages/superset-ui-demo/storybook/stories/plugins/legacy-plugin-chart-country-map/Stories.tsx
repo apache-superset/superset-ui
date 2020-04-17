c68ce056bedebef480a32533803fc41f44019a78
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 15:53:50 -0700

    docs: update storybook

--- packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-country-map/Stories.tsx
+++ packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-country-map/Stories.tsx
@@ -2,25 +2,26 @@
 import React from 'react';
 import { SuperChart } from '@superset-ui/chart';
 import { select } from '@storybook/addon-knobs';
+import CountryMapChartPlugin from '@superset-ui/legacy-plugin-chart-country-map';
+import countries from '@superset-ui/legacy-plugin-chart-country-map/esm/countries';
 import data from './data';
-import countries from '../../../../superset-ui-legacy-plugin-chart-country-map/esm/countries';
 
-export default [
-  {
-    renderStory: () => (
-      <SuperChart
-        chartType="country-map"
-        width={400}
-        height={400}
-        queryData={{ data }}
-        formData={{
-          linearColorScheme: 'schemeRdYlBu',
-          numberFormat: '.3s',
-          selectCountry: select('Country', Object.keys(countries!), 'france', 'country'),
-        }}
-      />
-    ),
-    storyName: 'Basic',
-    storyPath: 'legacy-|plugin-chart-country-map|CountryMapChartPlugin',
-  },
-];
+new CountryMapChartPlugin().configure({ key: 'country-map' }).register();
+
+export default {
+  title: 'Legacy Chart Plugins|legacy-plugin-chart-country-map',
+};
+
+export const basic = () => (
+  <SuperChart
+    chartType="country-map"
+    width={400}
+    height={400}
+    queryData={{ data }}
+    formData={{
+      linearColorScheme: 'schemeRdYlBu',
+      numberFormat: '.3s',
+      selectCountry: select('Country', Object.keys(countries!), 'france', 'country'),
+    }}
+  />
+);
