0eb0f62c2dbf6624d95b701bac235a96b6b0be8d
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-29 12:22:29 -0700

    fix: export

--- packages/superset-ui-control-utils/src/index.ts
+++ packages/superset-ui-control-utils/src/index.ts
@@ -1,3 +1,5 @@
-export * as sections from './sections';
+import * as sectionModules from './sections';
+
+export const sections = sectionModules;
 export { D3_FORMAT_DOCS, D3_FORMAT_OPTIONS, D3_TIME_FORMAT_OPTIONS } from './D3Formatting';
 export { formatSelectOptions, formatSelectOptionsForRange } from './selectOptions';
