fad0d89505367b44878200ef516d279a1f78ad08
Evan Rusackas <evan@preset.io>
2020-05-06 12:35:15 -0700

    style: removing .ts, ignoring lint warning

--- plugins/legacy-plugin-chart-sunburst/src/index.js
+++ plugins/legacy-plugin-chart-sunburst/src/index.js
@@ -20,7 +20,8 @@ import { t } from '@superset-ui/translation';
 import { ChartMetadata, ChartPlugin } from '@superset-ui/chart';
 import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
-import controlPanel from './controlPanel.ts';
+// eslint-disable-next-line import/extensions
+import controlPanel from './controlPanel';
 
 const metadata = new ChartMetadata({
   credits: ['https://bl.ocks.org/kerryrodden/7090426'],
