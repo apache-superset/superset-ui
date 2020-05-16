7ef1c36e7f0a3344c5e0f23b475eea3de09f2520
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-05-15 18:47:07 -0700

    fix: update format

--- packages/superset-ui-time-format/src/TimeFormatsForGranularity.ts
+++ packages/superset-ui-time-format/src/TimeFormatsForGranularity.ts
@@ -1,10 +1,8 @@
 import TimeFormats from './TimeFormats';
 import { TimeGranularity } from './types';
 
-const MINUTE = '%Y-%m-%d %H:%M';
-const SUNDAY_BASED_WEEK = '%Y-%m-%d W%U';
-const MONDAY_BASED_WEEK = '%Y-%m-%d W%W';
 const { DATABASE_DATE, DATABASE_DATETIME } = TimeFormats;
+const MINUTE = '%Y-%m-%d %H:%M';
 
 /**
  * Map time granularity to d3-format string
@@ -19,14 +17,14 @@ const TimeFormatsForGranularity: Record<TimeGranularity, string> = {
   [TimeGranularity.HALF_HOUR]: MINUTE,
   [TimeGranularity.HOUR]: '%Y-%m-%d %H:00',
   [TimeGranularity.DAY]: DATABASE_DATE,
-  [TimeGranularity.WEEK]: SUNDAY_BASED_WEEK,
+  [TimeGranularity.WEEK]: DATABASE_DATE,
   [TimeGranularity.MONTH]: '%Y-%m',
   [TimeGranularity.QUARTER]: '%Y Q%q',
   [TimeGranularity.YEAR]: '%Y',
-  [TimeGranularity.WEEK_STARTING_SUNDAY]: SUNDAY_BASED_WEEK,
-  [TimeGranularity.WEEK_STARTING_MONDAY]: MONDAY_BASED_WEEK,
-  [TimeGranularity.WEEK_ENDING_SATURDAY]: SUNDAY_BASED_WEEK,
-  [TimeGranularity.WEEK_ENDING_SUNDAY]: MONDAY_BASED_WEEK,
+  [TimeGranularity.WEEK_STARTING_SUNDAY]: DATABASE_DATE,
+  [TimeGranularity.WEEK_STARTING_MONDAY]: DATABASE_DATE,
+  [TimeGranularity.WEEK_ENDING_SATURDAY]: DATABASE_DATE,
+  [TimeGranularity.WEEK_ENDING_SUNDAY]: DATABASE_DATE,
 };
 
 export default TimeFormatsForGranularity;
