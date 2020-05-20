2150aa8197a39db10216e7dbe93da515415c0616
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-05-20 14:36:32 -0700

    refactor: improve end time computation to be daylight saving compatible

--- packages/superset-ui-time-format/src/utils/createTimeRangeFromGranularity.ts
+++ packages/superset-ui-time-format/src/utils/createTimeRangeFromGranularity.ts
@@ -4,31 +4,6 @@ import createTime from './createTime';
 const MS_IN_SECOND = 1000;
 const MS_IN_MINUTE = 60 * MS_IN_SECOND;
 const MS_IN_HOUR = 60 * MS_IN_MINUTE;
-const MS_IN_DAY = 24 * MS_IN_HOUR;
-const MS_IN_WEEK = 7 * MS_IN_DAY;
-
-const Offsets: Record<
-  Exclude<
-    TimeGranularity,
-    typeof TimeGranularity.MONTH | typeof TimeGranularity.QUARTER | typeof TimeGranularity.YEAR
-  >,
-  number
-> = {
-  [TimeGranularity.DATE]: MS_IN_DAY - 1,
-  [TimeGranularity.SECOND]: MS_IN_SECOND - 1,
-  [TimeGranularity.MINUTE]: MS_IN_MINUTE - 1,
-  [TimeGranularity.FIVE_MINUTES]: MS_IN_MINUTE * 5 - 1,
-  [TimeGranularity.TEN_MINUTES]: MS_IN_MINUTE * 10 - 1,
-  [TimeGranularity.FIFTEEN_MINUTES]: MS_IN_MINUTE * 15 - 1,
-  [TimeGranularity.HALF_HOUR]: MS_IN_MINUTE * 30 - 1,
-  [TimeGranularity.HOUR]: MS_IN_HOUR - 1,
-  [TimeGranularity.DAY]: MS_IN_DAY - 1,
-  [TimeGranularity.WEEK]: MS_IN_WEEK - 1,
-  [TimeGranularity.WEEK_STARTING_SUNDAY]: MS_IN_WEEK - 1,
-  [TimeGranularity.WEEK_STARTING_MONDAY]: MS_IN_WEEK - 1,
-  [TimeGranularity.WEEK_ENDING_SATURDAY]: -MS_IN_WEEK + 1,
-  [TimeGranularity.WEEK_ENDING_SUNDAY]: -MS_IN_WEEK + 1,
-};
 
 function deductOneMs(time: Date) {
   return new Date(time.getTime() - 1);
@@ -36,30 +11,52 @@ function deductOneMs(time: Date) {
 
 function computeEndTimeFromGranularity(
   time: Date,
-  granularity:
-    | typeof TimeGranularity.MONTH
-    | typeof TimeGranularity.QUARTER
-    | typeof TimeGranularity.YEAR,
+  granularity: TimeGranularity,
   useLocalTime: boolean,
 ) {
+  const date = useLocalTime ? time.getDate() : time.getUTCDate();
   const month = useLocalTime ? time.getMonth() : time.getUTCMonth();
   const year = useLocalTime ? time.getFullYear() : time.getUTCFullYear();
   const mode = useLocalTime ? 'local' : 'utc';
 
-  if (granularity === TimeGranularity.MONTH) {
-    return deductOneMs(
-      month === 11 ? createTime(mode, year + 1) : createTime(mode, year, month + 1),
-    );
+  switch (granularity) {
+    case TimeGranularity.SECOND:
+      return new Date(time.getTime() + MS_IN_SECOND - 1);
+    case TimeGranularity.MINUTE:
+      return new Date(time.getTime() + MS_IN_MINUTE - 1);
+    case TimeGranularity.FIVE_MINUTES:
+      return new Date(time.getTime() + MS_IN_MINUTE * 5 - 1);
+    case TimeGranularity.TEN_MINUTES:
+      return new Date(time.getTime() + MS_IN_MINUTE * 10 - 1);
+    case TimeGranularity.FIFTEEN_MINUTES:
+      return new Date(time.getTime() + MS_IN_MINUTE * 15 - 1);
+    case TimeGranularity.HALF_HOUR:
+      return new Date(time.getTime() + MS_IN_MINUTE * 30 - 1);
+    case TimeGranularity.HOUR:
+      return new Date(time.getTime() + MS_IN_HOUR - 1);
+    // For the day granularity and above, using Date overflow is better than adding timestamp
+    // because it will also handle daylight saving.
+    case TimeGranularity.WEEK:
+    case TimeGranularity.WEEK_STARTING_SUNDAY:
+    case TimeGranularity.WEEK_STARTING_MONDAY:
+      return deductOneMs(createTime(mode, year, month, date + 7));
+    case TimeGranularity.MONTH:
+      return deductOneMs(createTime(mode, year, month + 1));
+    case TimeGranularity.QUARTER:
+      return deductOneMs(createTime(mode, year, (Math.floor(month / 3) + 1) * 3));
+    case TimeGranularity.YEAR:
+      return deductOneMs(createTime(mode, year + 1));
+    // For the WEEK_ENDING_XXX cases,
+    // currently assume "time" returned from database is supposed to be the end time
+    // (in contrast to all other granularities that the returned time is start time).
+    // However, the returned "time" is at 00:00:00.000, so have to add 23:59:59.999.
+    case TimeGranularity.WEEK_ENDING_SATURDAY:
+    case TimeGranularity.WEEK_ENDING_SUNDAY:
+    case TimeGranularity.DATE:
+    case TimeGranularity.DAY:
+    default:
+      return deductOneMs(createTime(mode, year, month, date + 1));
   }
-  if (granularity === TimeGranularity.QUARTER) {
-    const quarter = Math.floor(month / 3) + 1;
-    return deductOneMs(
-      quarter <= 3 ? createTime(mode, year, quarter * 3) : createTime(mode, year + 1),
-    );
-  }
-
-  // Year
-  return deductOneMs(createTime(mode, year + 1));
 }
 
 export default function createTimeRangeFromGranularity(
@@ -67,24 +64,18 @@ export default function createTimeRangeFromGranularity(
   granularity: TimeGranularity,
   useLocalTime: boolean = false,
 ) {
+  const endTime = computeEndTimeFromGranularity(time, granularity, useLocalTime);
+
   if (
-    granularity === TimeGranularity.MONTH ||
-    granularity === TimeGranularity.QUARTER ||
-    granularity === TimeGranularity.YEAR
+    granularity === TimeGranularity.WEEK_ENDING_SATURDAY ||
+    granularity === TimeGranularity.WEEK_ENDING_SUNDAY
   ) {
-    return [time, computeEndTimeFromGranularity(time, granularity, useLocalTime)];
-  }
-  const offset = Offsets[granularity];
-
-  if (offset >= 0) {
-    const offsetTime = new Date(time.getTime() + offset);
-    return [time, offsetTime];
+    const date = useLocalTime ? time.getDate() : time.getUTCDate();
+    const month = useLocalTime ? time.getMonth() : time.getUTCMonth();
+    const year = useLocalTime ? time.getFullYear() : time.getUTCFullYear();
+    const startTime = createTime(useLocalTime ? 'local' : 'utc', year, month, date - 6);
+    return [startTime, endTime];
   }
 
-  // For the WEEK_ENDING_XXX cases,
-  // currently assume given time is at 00:00:00.000 from database
-  // so add 23:59:59.999
-  const endTime = new Date(time.getTime() + MS_IN_DAY - 1);
-  const offsetTime = new Date(endTime.getTime() + offset);
-  return [offsetTime, endTime];
+  return [time, endTime];
 }
