880ffc81ba7aeee5a8d55ea2f73a93c1f8b2e32f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-05-22 14:47:35 -0700

    feat(plugin-chart-choropleth-map): scaffold and load map (#527)
    * feat: add package
    
    * feat: storybook working
    
    * feat: load usa and world map
    
    * refactor: clean up
    
    * fix: remove test data
    
    * refactor: utilize dynamic import
    
    * build: remove unused dependencies
    
    * fix: address pr comments
    
    * fix: comment

---
+++ packages/superset-ui-demo/storybook/stories/plugins/plugin-chart-choropleth-map/Stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+import {
+  maps,
+  ChoroplethMapChartPlugin,
+} from '../../../../../../plugins/plugin-chart-choropleth-map/src';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import data from './Stories';
+
+new ChoroplethMapChartPlugin().configure({ key: 'choropleth-map' }).register();
+
+export default {
+  title: 'Chart Plugins|plugin-chart-choropleth-map',
+  decorators: [withKnobs],
+};
+
+export const worldMap = () => (
+  <SuperChart
+    chartType="choropleth-map"
+    width={800}
+    height={450}
+    queryData={{ data }}
+    formData={{
+      map: select(
+        'Map',
+        maps.map(m => m.key),
+        'world',
+        'map',
+      ),
+    }}
+  />
+);
+
+export const usa = () => (
+  <SuperChart
+    chartType="choropleth-map"
+    width={800}
+    height={450}
+    queryData={{ data }}
+    formData={{
+      map: 'usa',
+    }}
+  />
+);
