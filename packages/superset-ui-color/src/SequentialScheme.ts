a0714677a1b918548adf319536b4dff5dbf313d6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-05-27 13:01:04 -0700

    feat: update functions

--- packages/superset-ui-color/src/SequentialScheme.ts
+++ packages/superset-ui-color/src/SequentialScheme.ts
@@ -1,4 +1,5 @@
 import { scaleLinear } from 'd3-scale';
+import { interpolateHcl } from 'd3-interpolate';
 import ColorScheme, { ColorSchemeConfig } from './ColorScheme';
 
 function range(count: number) {
@@ -23,24 +24,68 @@ export default class SequentialScheme extends ColorScheme {
     this.isDiverging = isDiverging;
   }
 
-  createLinearScale(extent: number[] = [0, 1]) {
-    // Create matching domain
-    // because D3 continuous scale uses piecewise mapping
-    // between domain and range.
-    const valueScale = scaleLinear().range(extent);
+  /**
+   * Create a linear scale using the colors in this scheme as a range
+   * and interpolate domain [0, 1]
+   * to match the number of elements in the range
+   * e.g.
+   * If the range is ['red', 'white', 'blue']
+   * the domain of this scale will be
+   * [0, 0.5, 1]
+   */
+  private createPiecewiseScale() {
     const denominator = this.colors.length - 1;
-    const domain = range(this.colors.length).map(i => valueScale(i / denominator));
 
-    return scaleLinear<string>().domain(domain).range(this.colors).clamp(true);
+    return scaleLinear<string>()
+      .domain(range(this.colors.length).map(i => i / denominator))
+      .range(this.colors)
+      .interpolate(interpolateHcl)
+      .clamp(true);
   }
 
-  getColors(numColors: number = this.colors.length): string[] {
+  /**
+   * Return a linear scale with a new domain interpolated from the input domain
+   * to match the number of elements in the color scheme
+   * because D3 continuous scale uses piecewise mapping between domain and range.
+   * This is a common use-case when the domain is [min, max]
+   * and the palette such as ColorBrewer's has multiple elements.
+   *
+   * @param domain domain of the scale
+   * @param modifyRange Set this to true if you don't want to modify the domain and
+   * want to interpolate range to have the same number of elements with domain instead.
+   */
+  createLinearScale(domain: number[] = [0, 1], modifyRange = false) {
+    if (modifyRange || domain.length === this.colors.length) {
+      return scaleLinear<string>()
+        .interpolate(interpolateHcl)
+        .domain(domain)
+        .range(this.getColors(domain.length));
+    }
+
+    const piecewiseScale = this.createPiecewiseScale();
+    const adjustDomain = scaleLinear().range(domain).clamp(true);
+    const newDomain = piecewiseScale.domain().map(d => adjustDomain(d));
+
+    return piecewiseScale.domain(newDomain);
+  }
+
+  /**
+   * Get colors from this scheme
+   * @param numColors number of colors to return.
+   * Will interpolate the current scheme to match the number of colors requested
+   * @param extent The extent of the color range to use.
+   * For example [0.2, 1] will rescale the color scheme
+   * such that color values in the range [0, 0.2) are excluded from the scheme.
+   */
+  getColors(numColors: number = this.colors.length, extent: number[] = [0, 1]): string[] {
     if (numColors === this.colors.length) {
       return this.colors;
     }
-    const colorScale = this.createLinearScale();
-    const denominator = numColors - 1;
 
-    return range(numColors).map(i => colorScale(i / denominator));
+    const piecewiseScale = this.createPiecewiseScale();
+    const adjustExtent = scaleLinear().range(extent).clamp(true);
+    const denominator2 = numColors - 1;
+
+    return range(numColors).map(i => piecewiseScale(adjustExtent(i / denominator2)));
   }
 }
