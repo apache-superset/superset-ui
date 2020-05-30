cca63ef2cbea6ca40ee99544e36c4e48c61856f4
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-05-30 16:03:08 -0700

    fix: remove deprecated

--- packages/superset-ui-connection/src/withSupersetClient.ts
+++
@@ -1,7 +0,0 @@
-import { SupersetClientInterface } from './types';
-
-export default function withSupersetClient<T>(
-  func: ({ client, ...rest }: { client: SupersetClientInterface; [key: string]: unknown }) => T,
-) {
-  return (...args) => func(...args);
-}
