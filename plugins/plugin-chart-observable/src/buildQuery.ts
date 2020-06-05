57bb0abaf9604956c03d2d2b8a7b89a4837dfae7
Evan Rusackas <evan@preset.io>
2020-06-05 14:06:48 -0700

    flattened/simplified file structure.

--- plugins/plugin-chart-observable/src/buildQuery.ts
+++ plugins/plugin-chart-observable/src/buildQuery.ts
@@ -1,5 +1,5 @@
 import { buildQueryContext } from '@superset-ui/query';
-import { ObservableFormData } from '../types';
+import { ObservableFormData } from './types';
 
 export default function buildQuery(formData: ObservableFormData) {
   // Set the single QueryObject's groupby field with series in formData
--- plugins/plugin-chart-observable/src/transformProps.ts
+++ plugins/plugin-chart-observable/src/transformProps.ts
@@ -1,6 +1,6 @@
 import { ChartProps } from '@superset-ui/chart';
-import { ObservableProps } from '../chart/Observable';
-import { ObservableFormData } from '../types';
+import { ObservableProps } from './chart/Observable';
+import { ObservableFormData } from './types';
 
 export default function transformProps(chartProps: ChartProps): ObservableProps {
   const { width, height, formData, queryData } = chartProps;
@@ -10,8 +10,6 @@ export default function transformProps(chartProps: ChartProps): ObservableProps 
     dataInjectionCell,
     showDebug,
   } = formData as ObservableFormData;
-
-  // console.warn('formData!!!!!!!!!!!', formData);
 
   return {
     data: queryData.data,
