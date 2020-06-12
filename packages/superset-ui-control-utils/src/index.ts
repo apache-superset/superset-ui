0bbc738c039fa49d80cbbf5a889fae255a8b5460
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-06-11 21:23:56 -0700

    fix mr conflict

--- packages/superset-ui-control-utils/src/index.ts
+++ packages/superset-ui-control-utils/src/index.ts
@@ -8,7 +8,6 @@ export const internalSharedControls = sharedControlsModule;
 export const sections = sectionModules;
 export { D3_FORMAT_DOCS, D3_FORMAT_OPTIONS, D3_TIME_FORMAT_OPTIONS } from './D3Formatting';
 export { formatSelectOptions, formatSelectOptionsForRange } from './selectOptions';
-export { default as MetricOption } from './MetricOption';
 export * from './InfoTooltipWithTrigger';
 export * from './ColumnOption';
 export * from './ColumnTypeLabel';
