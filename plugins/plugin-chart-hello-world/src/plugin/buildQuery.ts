f851f371efd379c71666ebe6b806283f869738da
Evan Rusackas <evan@preset.io>
2020-06-15 23:02:03 -0700

    feat: hello world plugin

---
+++ plugins/plugin-chart-hello-world/src/plugin/buildQuery.ts
@@ -0,0 +1,11 @@
+import { buildQueryContext } from '@superset-ui/query';
+import { ObservableFormData } from './types';
+
+export default function buildQuery(formData: ObservableFormData) {
+  // Set the single QueryObject's groupby field with series in formData
+  return buildQueryContext(formData, baseQueryObject => [
+    {
+      ...baseQueryObject,
+    },
+  ]);
+}
---
+++ plugins/plugin-chart-hello-world/test/index.test.ts
@@ -0,0 +1,7 @@
+import { HelloWorldChartPlugin } from '../src';
+
+describe('@superset-ui/plugin-chart-hello-world', () => {
+  it('exists', () => {
+    expect(HelloWorldChartPlugin).toBeDefined();
+  });
+});
