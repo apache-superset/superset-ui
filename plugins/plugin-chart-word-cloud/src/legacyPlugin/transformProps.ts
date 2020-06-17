8e32453d78e6cd371c606ff1cdd116f2c2b1ce87
Evan Rusackas <evan@preset.io>
2020-06-17 10:43:52 -0700

    nixing word cloud console logs

--- plugins/plugin-chart-word-cloud/src/legacyPlugin/transformProps.ts
+++ plugins/plugin-chart-word-cloud/src/legacyPlugin/transformProps.ts
@@ -15,8 +15,6 @@ function getMetricLabel(metric: LegacyWordCloudFormData['metric']): string | und
 
 export default function transformProps(chartProps: ChartProps): WordCloudProps {
   const { width, height, formData, queryData } = chartProps;
-  console.log('!!!formData', formData);
-  console.log('!!!queryData', queryData);
   const {
     colorScheme,
     metric,
