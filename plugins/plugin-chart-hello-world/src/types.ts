4c34ae60f1b3268cfd62b73ba3c40de24b79d638
Evan Rusackas <evan@preset.io>
2020-06-26 15:07:27 -0700

    typescript comments, minor formatting

--- plugins/plugin-chart-hello-world/src/types.ts
+++ plugins/plugin-chart-hello-world/src/types.ts
@@ -20,9 +20,21 @@ import { QueryFormData } from '@superset-ui/query';
 import { HelloWorldProps } from './HelloWorld';
 
 /**
+ * Note: All of Superset is in the process of migrating to TypeScript!
+ * For more information, see SIP-36:
+ * https://github.com/apache/incubator-superset/issues/9101
+ *
+ * If you are not comfortable with Typescript, and do not intend to
+ * open a pull request with your new plugin, feel free to do one
+ * of the following:
+ * - Convert your .ts(x) files to .js(x) to opt out completely
+ * - Use "// @ts-ignore" comments for single lines, or "// @ts-nocheck"
+ *   at the top of a file to opt out of type checking completely.
+ */
+
+/**
  * FormData contains both common properties of all form data
  * and properties specific to HelloWorld visualization
- * TODO: add links to TypeScript SIP, TS docs, handy @ts-ignore comments
  */
 export type HelloWorldFormData = QueryFormData &
   HelloWorldProps & {
