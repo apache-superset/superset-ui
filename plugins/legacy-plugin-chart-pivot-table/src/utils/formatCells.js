8ef584e59039969eec27d5b2cbfad1403946d6f4
mayur <mayurnewase111@gmail.com>
2020-12-24 00:36:25 +0530

    fix(legacy-plugin-chart-pivot-table): empty value for string aggregate

---
+++ plugins/legacy-plugin-chart-pivot-table/src/utils/formatCells.js
@@ -0,0 +1,38 @@
+import { formatNumber } from '@superset-ui/core';
+
+function eachRow(i, cols, tdText, columnFormats, numberFormat, dateRegex, dateFormatter) {
+  const metric = cols[i];
+  const format = columnFormats[metric] || numberFormat || '.3s';
+  const parsedValue = parseFloat(tdText);
+  var textContent, attr;
+
+  if (Number.isNaN(parsedValue)) {
+    const regexMatch = dateRegex.exec(tdText);
+    if (regexMatch) {
+      const date = new Date(parseFloat(regexMatch[1]));
+      textContent = dateFormatter(date);
+      attr = ('data-sort', date);
+    } else {
+      textContent = tdText;
+      attr = ('data-sort', tdText);
+    }
+  } else {
+    textContent = formatNumber(format, parsedValue);
+    attr = ('data-sort', parsedValue);
+  }
+  return { textContent: textContent, attr: attr };
+}
+
+function replace(text, verboseMap, dateRegex, dateFormatter) {
+  const regexMatch = dateRegex.exec(text);
+  let cellValue;
+  if (regexMatch) {
+    const date = new Date(parseFloat(regexMatch[1]));
+    cellValue = dateFormatter(date);
+  } else {
+    cellValue = verboseMap[text] || text;
+  }
+  return cellValue;
+}
+
+export { eachRow, replace };
---
+++ plugins/legacy-plugin-chart-pivot-table/test/PivotTable.test.tsx
@@ -0,0 +1,57 @@
+import { getTimeFormatter, getTimeFormatterForGranularity } from '@superset-ui/core';
+import { eachRow, replace } from '../src/utils/formatCells';
+
+describe('pivot table plugin format cells', () => {
+  let i = 0;
+  let cols = ['SUM'];
+  let tdText = '2222222';
+  let columnFormats = {};
+  let numberFormat = 'SMART_NUMBER';
+  let dateRegex = /^__timestamp:(-?\d*\.?\d*)$/;
+  let dateFormatter = getTimeFormatterForGranularity('P1D');
+
+  it('render number', () => {
+    const { textContent, attr } = eachRow(
+      i,
+      cols,
+      tdText,
+      columnFormats,
+      numberFormat,
+      dateRegex,
+      dateFormatter,
+    );
+    expect(textContent).toEqual('2.22M');
+    expect(attr).toEqual(('data-sort', 2222222));
+  });
+
+  it('render date', () => {
+    tdText = '__timestamp:-126230400000.0';
+
+    const { textContent, attr } = eachRow(
+      i,
+      cols,
+      tdText,
+      columnFormats,
+      numberFormat,
+      dateRegex,
+      dateFormatter,
+    );
+    expect(textContent).toEqual('1966-01-01');
+  });
+
+  it('render string', () => {
+    tdText = 'some-text';
+
+    const { textContent, attr } = eachRow(
+      i,
+      cols,
+      tdText,
+      columnFormats,
+      numberFormat,
+      dateRegex,
+      dateFormatter,
+    );
+    expect(textContent).toEqual(tdText);
+    expect(attr).toEqual(('data-sort', tdText));
+  });
+});
