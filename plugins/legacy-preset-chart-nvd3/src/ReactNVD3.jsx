09fc66f18e0ca43923308939acaf830ab9dff3ae
Victor Malai <malai9696@gmail.com>
2021-01-20 09:44:24 +0200

    Apply scroll only bar chart

--- plugins/legacy-preset-chart-nvd3/src/ReactNVD3.jsx
+++ plugins/legacy-preset-chart-nvd3/src/ReactNVD3.jsx
@@ -46,7 +46,7 @@ NVD3.propTypes = {
 };
 
 export default styled(NVD3)`
-  .superset-legacy-chart-nvd3-dist-bar, .superset-legacy-chart-nvd3 {
+  .superset-legacy-chart-nvd3-dist-bar, .superset-legacy-chart-nvd3-bar {
     overflow-x: auto !important;
     svg {
       &.nvd3-svg {
