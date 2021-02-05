61e21023445c6ace9d715180ee19e63b886a83f2
Victor Malai <malai9696@gmail.com>
2021-02-05 11:19:38 +0200

    feat(plugin-chart-word-cloud): subject Add order by desc

--- plugins/plugin-chart-word-cloud/src/plugin/buildQuery.ts
+++ plugins/plugin-chart-word-cloud/src/plugin/buildQuery.ts
@@ -3,9 +3,11 @@ import { WordCloudFormData } from '../types';
 
 export default function buildQuery(formData: WordCloudFormData) {
   // Set the single QueryObject's groupby field with series in formData
+  const { metric } = formData;
   return buildQueryContext(formData, baseQueryObject => [
     {
       ...baseQueryObject,
+      orderby: [[metric, false]],
     },
   ]);
 }
