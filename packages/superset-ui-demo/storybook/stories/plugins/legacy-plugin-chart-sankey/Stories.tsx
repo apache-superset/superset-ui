799e4b295e4c374702d1358366a52d9525d51578
Victor Malai <malai9696@gmail.com>
2021-03-05 15:15:06 +0200

    Resizable in separate page

--- packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sankey/Stories.tsx
+++ packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sankey/Stories.tsx
@@ -2,24 +2,39 @@
 import React from 'react';
 import { SuperChart } from '@superset-ui/core';
 import SankeyChartPlugin from '@superset-ui/legacy-plugin-chart-sankey';
-import { withResizableChartDemo } from '../../../shared/components/ResizableChartDemo';
+import ResizableChartDemo from '../../../shared/components/ResizableChartDemo';
 import data from './data';
 
 new SankeyChartPlugin().configure({ key: 'sankey' }).register();
 
 export default {
   title: 'Legacy Chart Plugins|legacy-plugin-chart-sankey',
-  decorators: [withResizableChartDemo],
 };
 
-export const basic = ({ width, height }) => (
+export const basic = () => (
   <SuperChart
     chartType="sankey"
-    width={width}
-    height={height}
+    width={400}
+    height={400}
     queriesData={[{ data }]}
     formData={{
       colorScheme: 'd3Category10',
     }}
   />
+);
+
+export const resizable = () => (
+  <ResizableChartDemo>
+    {({ width, height }) => (
+      <SuperChart
+        chartType="sankey"
+        width={width}
+        height={height}
+        queriesData={[{ data }]}
+        formData={{
+          colorScheme: 'd3Category10',
+        }}
+      />
+    )}
+  </ResizableChartDemo>
 );
--- packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sunburst/Stories.tsx
+++ packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sunburst/Stories.tsx
@@ -2,21 +2,20 @@
 import React from 'react';
 import { SuperChart } from '@superset-ui/core';
 import SunburstChartPlugin from '@superset-ui/legacy-plugin-chart-sunburst';
-import { withResizableChartDemo } from '../../../shared/components/ResizableChartDemo';
+import ResizableChartDemo from '../../../shared/components/ResizableChartDemo';
 import data from './data';
 
 new SunburstChartPlugin().configure({ key: 'sunburst' }).register();
 
 export default {
   title: 'Legacy Chart Plugins|legacy-plugin-chart-sunburst',
-  decorators: [withResizableChartDemo],
 };
 
-export const basic = ({ width, height }) => (
+export const basic = () => (
   <SuperChart
     chartType="sunburst"
-    width={width}
-    height={height}
+    width={400}
+    height={400}
     queriesData={[{ data }]}
     formData={{
       colorScheme: 'd3Category10',
@@ -24,4 +23,22 @@ export const basic = ({ width, height }) => (
       secondaryMetric: 'sum__SP_RUR_TOTL',
     }}
   />
+);
+
+export const resizable = () => (
+  <ResizableChartDemo>
+    {({ width, height }) => (
+      <SuperChart
+        chartType="sunburst"
+        width={width}
+        height={height}
+        queriesData={[{ data }]}
+        formData={{
+          colorScheme: 'd3Category10',
+          metric: 'sum__SP_POP_TOTL',
+          secondaryMetric: 'sum__SP_RUR_TOTL',
+        }}
+      />
+    )}
+  </ResizableChartDemo>
 );
