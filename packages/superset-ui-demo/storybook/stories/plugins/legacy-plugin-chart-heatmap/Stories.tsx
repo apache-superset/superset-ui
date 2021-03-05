6ea8223e0f1a05dd713c16eb6c723c23b6eb7117
Victor Malai <malai9696@gmail.com>
2021-03-05 15:11:52 +0200

    Resizable

--- packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-heatmap/Stories.tsx
+++ packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-heatmap/Stories.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { SuperChart } from '@superset-ui/core';
 import HeatmapChartPlugin from '@superset-ui/legacy-plugin-chart-heatmap';
+import ResizableChartDemo from '../../../shared/components/ResizableChartDemo';
 import data from './data';
-import { withResizableChartDemo } from '../../../shared/components/ResizableChartDemo';
 
 new HeatmapChartPlugin().configure({ key: 'heatmap' }).register();
 
 export default {
   title: 'Legacy Chart Plugins|legacy-plugin-chart-heatmap',
-  decorators: [withResizableChartDemo],
 };
 
-export const basic = ({ width, height }) => (
+export const basic = () => (
   <SuperChart
     chartType="heatmap"
-    width={width}
-    height={height}
+    width={500}
+    height={500}
     formData={{
       allColumnsX: 'source',
       allColumnsY: 'target',
@@ -46,11 +45,50 @@ export const basic = ({ width, height }) => (
   />
 );
 
-export const withNullData = ({ width, height }) => (
+export const resizable = () => (
+  <ResizableChartDemo>
+    {({ width, height }) => (
+      <SuperChart
+        chartType="heatmap"
+        width={width}
+        height={height}
+        formData={{
+          allColumnsX: 'source',
+          allColumnsY: 'target',
+          bottomMargin: 'auto',
+          canvasImageRendering: 'pixelated',
+          leftMargin: 'auto',
+          linearColorScheme: 'blue_white_yellow',
+          metric: 'sum__value',
+          normalized: false,
+          showLegend: true,
+          showPerc: true,
+          showValues: false,
+          sortXAxis: 'alpha_asc',
+          sortYAxis: 'alpha_asc',
+          xscaleInterval: '1',
+          yAxisBounds: [null, null],
+          yAxisFormat: '.3s',
+          yscaleInterval: '1',
+        }}
+        queriesData={[
+          {
+            data: {
+              records: data,
+              extents: [0.1, 24.9],
+            },
+          },
+        ]}
+      />
+    )}
+  </ResizableChartDemo>
+);
+
+export const withNullData = () => (
   <SuperChart
     chartType="heatmap"
-    width={width}
-    height={height}
+    width={500}
+    height={500}
     formData={{
       allColumnsX: 'source',
       allColumnsY: 'target',
