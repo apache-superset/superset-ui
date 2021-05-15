41e4cc9e867b945d11ade0198b9f65cdddb717ed
xiezhongfu <458957068@qq.com>
2021-05-15 21:08:06 +0800

    feat(plugin-chart-echarts): add SORT BY metrics to Radar

--- plugins/plugin-chart-echarts/src/Radar/buildQuery.ts
+++ plugins/plugin-chart-echarts/src/Radar/buildQuery.ts
@@ -16,14 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { buildQueryContext, QueryFormData } from '@superset-ui/core';
+import { buildQueryContext, QueryFormData, getMetricLabel } from '@superset-ui/core';
 
 export default function buildQuery(formData: QueryFormData) {
-  const { metric, sort_by_metric } = formData;
-  return buildQueryContext(formData, baseQueryObject => [
-    {
-      ...baseQueryObject,
-      ...(sort_by_metric && { orderby: [[metric, false]] }),
-    },
-  ]);
+  const { sort_by_metric } = formData;
+  return buildQueryContext(formData, baseQueryObject => {
+    const { metrics = [] } = baseQueryObject;
+    return [
+      {
+        ...baseQueryObject,
+        ...(sort_by_metric && { orderby: metrics.map(metric => [getMetricLabel(metric), false]) }),
+      },
+    ];
+  });
 }
