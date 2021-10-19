10d4ae03bc49819bc82850582789d52e36d5d11b
Hugh A. Miles II <hughmil3s@gmail.com>
2021-10-19 17:34:10 -0400

    Update SupersetClientClass.ts

--- packages/superset-ui-core/src/connection/SupersetClientClass.ts
+++ packages/superset-ui-core/src/connection/SupersetClientClass.ts
@@ -74,7 +74,7 @@ export default class SupersetClientClass {
     this.baseUrl = url.href.replace(/\/+$/, ''); // always strip trailing slash
     this.host = url.host;
     this.protocol = url.protocol as Protocol;
-    this.headers = { ...headers };
+    this.headers = { ...headers, 'Content-Type': 'application/json' };
     this.mode = mode;
     this.timeout = timeout;
     this.credentials = credentials;
